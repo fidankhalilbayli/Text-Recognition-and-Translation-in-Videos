dde7486e06c0929cf4d630a58a56a75dc3456fbb
Fidan Khalilbayli <34570004+fidankhalilbayli@users.noreply.github.com>
2020-01-14 14:04:07 +0300

    Add files via upload

--- text_recognition+translation.py
+++ text_recognition+translation.py
@@ -14,19 +14,17 @@ while(True):
 	for f in files:
 		image = cv2.imread(f, cv2.IMREAD_COLOR)
 		image = cv2.resize(image, (640, 640))
-		im  =  Image.new ( "RGB", (640, 640), (255,255,255))
-		draw  =  ImageDraw.Draw (im)
+		im  =  Image.new("RGB", (640, 640), (255,255,255))
+		draw  =  ImageDraw.Draw(im)
 		unicode_font = ImageFont.truetype("DejaVuSans.ttf", 15)
 		(H, W) = image.shape[:2]
 		board = np.zeros((H, W,3), np.uint8)
 		board[:] = 255
 		text = pytesseract.image_to_string(image, config="-l eng --oem 1 --psm 3")
 		random_text =text.splitlines()
-		
 		for i in range(len(random_text)):
 			text = translator.translate(random_text[i], dest='tr').text
 			text= u'%s'%(text)
-			#print (text)
 			draw.text ( (20,20*(i+1)), text, font=unicode_font, fill=(0,0,0) )
 		final_version = np.concatenate((im,image), axis=1)
 		cv2.imshow("Result", final_version)
@@ -36,7 +34,7 @@ while(True):
 		
 		if writer is None:
 			fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-			writer = cv2.VideoWriter("text_recognition1.avi", fourcc, 1,
+			writer = cv2.VideoWriter("text_recognition.avi", fourcc, 1,
 				(final_version.shape[1], final_version.shape[0]), True)
 		if writer is not None:
 			writer.write(final_version)	
